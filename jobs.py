a27f3495050c15a736a000c6079aa39f95ca8a82
Ilya Tkachev <skaatt@mail.ru>
2023-01-26 11:00:52 +0100

    fix too many reqs, add test run

--- jobs.py
+++ jobs.py
@@ -8,7 +8,8 @@ from tinvest import SyncClient
 from logger import log_start_end
 from stats import get_stats_for_period, _get_top_std_top_incr, _get_top_incr
 
-TIMEOUT = 3
+TELEGRAM_TIMEOUT = 3
+TINKOFF_SLEEP_SECONDS = 60
 
 USD = "usd"
 RUB = "rub"
@@ -31,7 +32,7 @@ def etf_stats_job():
     date_from = date_to - datetime.timedelta(days=days_to_analyze)
 
     etfs = client.get_market_etfs()
-    grouped_by_figi_stats = get_stats_for_period(client, etfs, date_from, date_to)
+    grouped_by_figi_stats = get_stats_for_period(client, etfs, date_from, date_to, sleep_seconds=TINKOFF_SLEEP_SECONDS)
     min_std_max_incr_usd = _get_top_std_top_incr(grouped_by_figi_stats, USD, top_std, True, top_incr, False)
     min_std_max_incr_rub = _get_top_std_top_incr(grouped_by_figi_stats, RUB, top_std, True, top_incr, False)
 
@@ -42,14 +43,14 @@ def etf_stats_job():
     max_incr_rub = _get_top_incr(grouped_by_figi_stats, RUB, top_incr, False)
 
     welcome_message = f"*Здоров, котаны. Свежая статистика по ETF за период {date_from.date()} - {date_to.date()}:*"
-    bot.send_message(text=welcome_message, parse_mode=PARSEMODE_MARKDOWN, chat_id=INVEST_CLUB_CHAT_ID, timeout=TIMEOUT)
+    bot.send_message(text=welcome_message, parse_mode=PARSEMODE_MARKDOWN, chat_id=INVEST_CLUB_CHAT_ID, timeout=TELEGRAM_TIMEOUT)
 
     rub = [
         (f"\n\nRUB Min std {top_std}, max incr", min_std_max_incr_rub),
         (f"\n\nRUB Max std {top_std}, max incr", max_std_max_incr_rub),
         (f"\n\nRUB Max incr", max_incr_rub),
     ]
-    bot.send_message(text="*Статистика по rub фондам:*", parse_mode=PARSEMODE_MARKDOWN, chat_id=INVEST_CLUB_CHAT_ID, timeout=TIMEOUT)
+    bot.send_message(text="*Статистика по rub фондам:*", parse_mode=PARSEMODE_MARKDOWN, chat_id=INVEST_CLUB_CHAT_ID, timeout=TELEGRAM_TIMEOUT)
     for message, stats in rub:
         _send_screenshot_with_caption(bot, message, stats)
 
@@ -58,7 +59,7 @@ def etf_stats_job():
         (f"\n\nUSD Max std {top_std}, max incr", max_std_max_incr_usd),
         (f"\n\nUSD Max incr", max_incr_usd),
     ]
-    bot.send_message(text="*Статистика по usd фондам:*", parse_mode=PARSEMODE_MARKDOWN, chat_id=INVEST_CLUB_CHAT_ID, timeout=TIMEOUT)
+    bot.send_message(text="*Статистика по usd фондам:*", parse_mode=PARSEMODE_MARKDOWN, chat_id=INVEST_CLUB_CHAT_ID, timeout=TELEGRAM_TIMEOUT)
     for message, stats in usd:
         _send_screenshot_with_caption(bot, message, stats)
 
@@ -73,14 +74,14 @@ def stocks_stats_job():
     date_from = date_to - datetime.timedelta(days=days_to_analyze)
 
     stocks = client.get_market_stocks()
-    grouped_by_figi_stats = get_stats_for_period(client, stocks, date_from, date_to, sleep_seconds=60)
+    grouped_by_figi_stats = get_stats_for_period(client, stocks, date_from, date_to, sleep_seconds=TINKOFF_SLEEP_SECONDS)
 
     max_incr_usd = _get_top_incr(grouped_by_figi_stats, USD, top_incr, False)
     max_incr_rub = _get_top_incr(grouped_by_figi_stats, RUB, top_incr, False)
 
     welcome_message = f"*Здоров, котаны. Свежая статистика по акциям(топ-{top_incr})" \
                       f"за период {date_from.date()} - {date_to.date()}:*"
-    bot.send_message(text=welcome_message, parse_mode=PARSEMODE_MARKDOWN, chat_id=INVEST_CLUB_CHAT_ID, timeout=TIMEOUT)
+    bot.send_message(text=welcome_message, parse_mode=PARSEMODE_MARKDOWN, chat_id=INVEST_CLUB_CHAT_ID, timeout=TELEGRAM_TIMEOUT)
 
     _send_screenshot_with_caption(bot, f"\n\nRUB Max incr", max_incr_rub)
     _send_screenshot_with_caption(bot, f"\n\nUSD Max incr", max_incr_usd)
@@ -90,5 +91,5 @@ def _send_screenshot_with_caption(bot: Bot, message: str, stats: pd.DataFrame):
     png = "stats.png"
     dataframe_image.export(stats, png, table_conversion="matplotlib")
     with open(png, "rb") as file:
-        bot.send_photo(photo=file, caption=f"_{message}_", parse_mode=PARSEMODE_MARKDOWN, chat_id=INVEST_CLUB_CHAT_ID, timeout=TIMEOUT)
+        bot.send_photo(photo=file, caption=f"_{message}_", parse_mode=PARSEMODE_MARKDOWN, chat_id=INVEST_CLUB_CHAT_ID, timeout=TELEGRAM_TIMEOUT)
     os.remove(png)
